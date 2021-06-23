9935d0b5dd7b5699a5467b9d10a76d97b8435e51
Scala Steward <me@scala-steward.org>
2021-06-23 16:50:35 +0200

    Update weaver-cats, weaver-scalacheck to 0.7.4

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ object Version {
   val catsCore = "2.6.1"
   val catsEffect = "3.1.1"
   val kindProjector = "0.13.0"
-  val weaver = "0.7.3"
+  val weaver = "0.7.4"
   val libGDX = "1.10.0"
   val fs2 = "3.0.4"
   val organizeImports = "0.5.0"
